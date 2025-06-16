4f874f24ce02231f76a5087de56c868e78e98ce5
ARTempest <artempest.gamer@gmail.com>
2025-06-15 23:28:30 -0400

    tokenized input
    The input from a test_file is now tokenizable into multiple tokens

--- tokenizer.s
+++ tokenizer.s
@@ -14,9 +14,6 @@ input_buffer:
 token_ptrs_buffer:
   .space 8192       # enough space for up to 1024 token addresses
 
-token_type_buffer:
-  .space 4096       # enough space for up to 1024 token type integers
-
 .section .text
 .globl _start
 
@@ -29,7 +26,7 @@ open_file:
   movq $0, %rsi
   syscall
 
-  xor %r14, %r14    # Will hold the amount of characters read from the file
+  xor %r14, %r14    # Will hold the amount of bytes read from the file
   xor %r15, %r15    # Will hold the file descriptor
 
   movq %rax, %r15   # save the file descriptor in %r15
@@ -37,9 +34,10 @@ open_file:
 read_loop:
   movq $0, %rax
   movq %r15, %rdi
-  leaq input_buffer(%rip), %rsi
+  movq $input_buffer, %rsi
   addq %r14, %rsi
   movq $65536, %rdx
+  subq %r14, %rdx
 
   syscall
 
@@ -48,18 +46,93 @@ read_loop:
   js exit
 
   addq %rax, %r14
-  jmp read_loop
 
 end_reading:
+  # Clean used registers
+  xor %rax, %rax
+  xor %r13, %r13
+  xor %r8, %r8                # will be used to hold the amount of bytes of a token
+  xor %r9, %r9                # will be used to count the amount of tokens
+  
+  xor %r10, %r10              # will point to token_ptrs
+  leaq token_ptrs_buffer(%rip), %r10
 
-
+  movq $input_buffer, %rsi   # point to the beggining of the buffer
+  leaq (%rsi, %r14), %r13    # point to the end of the buffer  
+  
 token_loop:
+  cmpq %r13, %rsi           # Check if reached the end of bytes readen
+  je exit
+
+  movb (%rsi), %al
+
+  cmpb $0, %al
+  je exit
+
+scan_byte:        # If char is a separation char just subtitute it for a delimiter \0
+  cmpb $' ', %al
+  je add_delimiter
+
+  cmpb $'\t', %al
+  je add_delimiter
+
+  jmp add_byte
+
+add_byte:
+  incq %r8        # increase offset
+  
+  cmpb $'\n', %al
+  je end_line
+
+next_byte:      # Increase the index in one
+  incq %rsi
+  jmp token_loop
+
+add_delimiter:
+  movb $0, (%rsi)
+  jmp end_line
+
+end_line:
+  cmpq $1, %r8
+  jne end_token
+
+  jmp next_byte
+
 
 end_token:
+  movl MAX_TOKENS(%rip), %ecx
+  cmpl %ecx, %r9d
+  je exit_overflow
 
-check_token_type:
+  # Move beginning of the token address to token_ptrs_buffer closest available spot
+  movq %rsi, %rax
+  subq %r8, %rax
+  movq %rax, (%r10)
 
-print_tokens:
+
+  addq $8, %r10 # point to the next available spot in token_ptrs_buffer
+  incl %r9d
+  xor %r8, %r8 # reset amount of bytes of the token
+  jmp token_loop
+
+close_file:
+  movq $3, %rax
+  movq %r15, %rdi
+
+  syscall
+
+  jmp exit
+
+exit_overflow:
+  movq $60, %rax
+  movq $1, %rdi
+
+  syscall
 
 exit:
+  movq $60, %rax
+  movq $0, %rdi
+
+  syscall
+
 
